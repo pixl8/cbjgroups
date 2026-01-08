910f5d5857c54b8fc89994fd0bec1256456d5a08
Dominic Watson <watson.dominic@gmail.com>
2026-01-08 11:44:13 +0000

    Quick attempt at Lucee 7 + jakarta compat

--- javaBuild.sh
+++ javaBuild.sh
@@ -8,7 +8,7 @@ mkdir -p artifacts
 mvn package || exit 1
 cp target/cbjgroups-1.0.0-jar-with-dependencies.jar artifacts/cbjgroups.jar
 cd artifacts
-unzip cbjgroups.jar
+unzip -q cbjgroups.jar
 echo "Manifest-Version: 1.0
 Bundle-ManifestVersion: 2
 Bundle-Name: CBJgroups Java Services
@@ -19,3 +19,22 @@ rm cbjgroups.jar
 zip -rq cbjgroups.jar *
 
 cp cbjgroups.jar ../../lib/
+
+cd ../../java-src-jakarta
+rm -rf artifacts
+mkdir -p artifacts
+
+mvn package || exit 1
+cp target/cbjgroups-jakarta-1.0.0-jar-with-dependencies.jar artifacts/cbjgroups-jakarta.jar
+cd artifacts
+unzip -q cbjgroups-jakarta.jar
+echo "Manifest-Version: 1.0
+Bundle-ManifestVersion: 2
+Bundle-Name: CBJgroups Java Services
+Bundle-SymbolicName: org.pixl8.cbjgroups
+Bundle-Version: 1.0.0
+" > META-INF/MANIFEST.MF
+rm cbjgroups-jakarta.jar
+zip -rq cbjgroups-jakarta.jar *
+
+cp cbjgroups-jakarta.jar ../../lib/
